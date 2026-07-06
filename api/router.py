d6129b0f6e4f2be0e64db09e36cd97037cd58524
M.I. Paulo Daniel Vázquez Mora <73305449+danielvazquez-utp@users.noreply.github.com>
2026-07-06 15:28:03 +0000

    Rutas de usuarios agregados

--- api/router.py
+++ api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.endpoints.usuario import router as usuario_router
+from api.endpoints.usuarios import router as usuario_router
 
 router = APIRouter()
 router.include_router(usuario_router)
